73042efaae053650db7e298392d5cac37b79ed74
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-06-25 04:06:50 +0100

    uncomment REI

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -36,7 +36,7 @@ void init(rack::Plugin *p) {
 	// p->addModel(modelHCTIP);
 	p->addModel(modelCURT);
 	// p->addModel(modelDFUZE);
-	// p->addModel(modelREI);
+	p->addModel(modelREI);
 	// p->addModel(modelRABBIT);
 	p->addModel(modelBISTROT);
 	p->addModel(modelSIGMA);
