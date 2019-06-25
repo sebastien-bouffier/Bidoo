963411a1d7ea5ca39f1d5cc513caca13278b77d3
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-06-25 18:24:57 +0100

    uncomment Perco

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -29,7 +29,7 @@ void init(rack::Plugin *p) {
 	// p->addModel(modelPENEQUE);
 	// p->addModel(modelLIMONADE);
 	// p->addModel(modelLIMBO);
-	// p->addModel(modelPERCO);
+	p->addModel(modelPERCO);
 	// p->addModel(modelFFILTR);
 	p->addModel(modelBAR);
 	p->addModel(modelZINC);
