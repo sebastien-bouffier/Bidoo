690f49a925b31338e3559b36ec5d7ef16403d20e
Phil Golden <12150421+Coirt@users.noreply.github.com>
2019-06-27 03:33:39 +0100

    HCTIP v1... see line 50
    Not sure the ternary on line 50 is having any effect on the outcome. Input * 5 * (1 or 0) is equal to Input * 5. could probably lose the ternary.

--- src/HCTIP.cpp
+++ src/HCTIP.cpp
@@ -1,81 +1,83 @@
-// #include "Bidoo.hpp"
-// #include "BidooComponents.hpp"
-// #include "dsp/digital.hpp"
-// #include "dsp/ringbuffer.hpp"
-// #include "dep/filters/pitchshifter.h"
-//
-// #define BUFF_SIZE 2048
-//
-// using namespace std;
-//
-// struct HCTIP : Module {
-// 	enum ParamIds {
-// 		PITCH_PARAM,
-// 		NUM_PARAMS
-// 	};
-// 	enum InputIds {
-// 		INPUT,
-// 		PITCH_INPUT,
-// 		NUM_INPUTS
-// 	};
-// 	enum OutputIds {
-// 		OUTPUT,
-// 		NUM_OUTPUTS
-// 	};
-// 	enum LightIds {
-// 		NUM_LIGHTS
-// 	};
-//
-// 	DoubleRingBuffer<float,BUFF_SIZE> in_Buffer;
-// 	DoubleRingBuffer<float,BUFF_SIZE> out_Buffer;
-// 	PitchShifter *pShifter = NULL;
-//
-// 	HCTIP() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {
-// 		pShifter = new PitchShifter(BUFF_SIZE, 8, engineGetSampleRate());
-// 	}
-//
-// 	~HCTIP() {
-// 		delete pShifter;
-// 	}
-//
-// 	void step() override;
-// };
-//
-//
-// void HCTIP::step() {
-// 	in_Buffer.push(inputs[INPUT].active ? inputs[INPUT].value/10.0f : 0.0f);
-//
-// 	if (in_Buffer.full()) {
-// 		pShifter->process(clamp(params[PITCH_PARAM].value + inputs[PITCH_INPUT].value ,0.5f,2.0f), in_Buffer.startData(), out_Buffer.endData());
-// 		out_Buffer.endIncr(BUFF_SIZE);
-// 		in_Buffer.clear();
-// 	}
-//
-// 	if (out_Buffer.size()>0) {
-// 		outputs[OUTPUT].value = *out_Buffer.startData() * 5.0f * (inputs[INPUT].active ? 1.0f : 0.0f);
-// 		out_Buffer.startIncr(1);
-// 	}
-//
-// }
-//
-// struct HCTIPWidget : ModuleWidget {
-// 	HCTIPWidget(HCTIP *module) : ModuleWidget(module) {
-// 		setPanel(SVG::load(assetPlugin(plugin, "res/HCTIP.svg")));
-//
-// 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-// 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-// 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-// 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-//
-//
-// 		addParam(ParamWidget::create<BidooBlueKnob>(Vec(8, 100), module, HCTIP::PITCH_PARAM, 0.5f, 2.0f, 1.0f));
-//
-// 		addInput(Port::create<PJ301MPort>(Vec(10, 150.66f), Port::INPUT, module, HCTIP::PITCH_INPUT));
-//
-// 		addInput(Port::create<PJ301MPort>(Vec(10, 242.66f), Port::INPUT, module, HCTIP::INPUT));
-//
-// 		addOutput(Port::create<PJ301MPort>(Vec(10, 299), Port::OUTPUT, module, HCTIP::OUTPUT));
-// 	}
-// };
-//
-// Model *modelHCTIP = Model::create<HCTIP, HCTIPWidget>("Bidoo", "HCTIP", "HCTIP pitch shifter", EFFECT_TAG);
+#include "plugin.hpp"
+#include "BidooComponents.hpp"
+#include "dsp/digital.hpp"
+#include "dsp/ringbuffer.hpp"
+#include "dep/filters/pitchshifter.h"
+
+#define BUFF_SIZE 2048
+
+using namespace std;
+
+struct HCTIP : Module {
+	enum ParamIds {
+		PITCH_PARAM,
+		NUM_PARAMS
+	};
+	enum InputIds {
+		INPUT,
+		PITCH_INPUT,
+		NUM_INPUTS
+	};
+	enum OutputIds {
+		OUTPUT,
+		NUM_OUTPUTS
+	};
+	enum LightIds {
+		NUM_LIGHTS
+	};
+
+	dsp::DoubleRingBuffer<float, BUFF_SIZE> in_Buffer;
+	dsp::DoubleRingBuffer<float, BUFF_SIZE> out_Buffer;
+	PitchShifter *pShifter = NULL;
+
+	HCTIP() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(PITCH_PARAM, 0.5f, 2.0f, 1.0f, "Pitch");
+
+		pShifter = new PitchShifter(BUFF_SIZE, 8, APP->engine->getSampleRate());
+	}
+
+	void process(const ProcessArgs &args) override {
+		in_Buffer.push(inputs[INPUT].isConnected() ? inputs[INPUT].getVoltage() / 10.0f : 0.0f);
+
+		if (in_Buffer.full()) {
+			pShifter->process(clamp(params[PITCH_PARAM].getValue() + inputs[PITCH_INPUT].getVoltage(), 0.5f, 2.0f), in_Buffer.startData(), out_Buffer.endData());
+			out_Buffer.endIncr(BUFF_SIZE);
+			in_Buffer.clear();
+		}
+
+		if (out_Buffer.size() > 0) {
+			outputs[OUTPUT].setVoltage(*out_Buffer.startData() * 5.0f * (inputs[INPUT].isConnected() ? 1.0f : 0.0f)); // x * 1 || 0 == x, disposable??
+			out_Buffer.startIncr(1);
+		}
+	}
+
+	~HCTIP() {
+		delete pShifter;
+	}
+
+
+};
+
+struct HCTIPWidget : ModuleWidget {
+	HCTIPWidget(HCTIP *module) {
+		setModule(module);
+		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/HCTIP.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+
+		addParam(createParam<BidooBlueKnob>(Vec(8, 100), module, HCTIP::PITCH_PARAM));
+
+		addInput(createInput<PJ301MPort>(Vec(10, 150.66f), module, HCTIP::PITCH_INPUT));
+
+		addInput(createInput<PJ301MPort>(Vec(10, 242.66f), module, HCTIP::INPUT));
+
+		addOutput(createOutput<PJ301MPort>(Vec(10, 299), module, HCTIP::OUTPUT));
+	}
+};
+
+Model *modelHCTIP = createModel<HCTIP, HCTIPWidget>("HCTIP");
