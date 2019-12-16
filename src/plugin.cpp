bdbdc9753400df36c2da0b01adcb86386944292b
sebastien-bouffier <sebastien.bouffier@gmail.com>
2019-12-16 11:48:10 +0100

    pic

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -6,26 +6,25 @@ Plugin *pluginInstance;
 void init(rack::Plugin *p) {
 	pluginInstance = p;
 
+	p->addModel(modelTOCANTE);
+	p->addModel(modelLATE);
 	p->addModel(modelDTROY);
 	p->addModel(modelBORDL);
 	p->addModel(modelZOUMAI);
 	p->addModel(modelMU);
-	p->addModel(modelTOCANTE);
   p->addModel(modelCHUTE);
-	p->addModel(modelLATE);
 	p->addModel(modelLOURDE);
 	p->addModel(modelACNE);
 	p->addModel(modelMS);
+	p->addModel(modelDUKE);
+	p->addModel(modelMOIRE);
 	p->addModel(modelOUAIVE);
 	p->addModel(modelPOUPRE);
 	p->addModel(modelMAGMA);
 	p->addModel(modelOAI);
 	p->addModel(modelCANARD);
 	p->addModel(modelEMILE);
-	p->addModel(modelDUKE);
-	p->addModel(modelMOIRE);
 	p->addModel(modelFORK);
-	p->addModel(modelGARCON);
 	p->addModel(modelTIARE);
 	p->addModel(modelCLACOS);
 	p->addModel(modelANTN);
@@ -45,5 +44,6 @@ void init(rack::Plugin *p) {
 	p->addModel(modelRABBIT);
 	p->addModel(modelBISTROT);
 	p->addModel(modelSIGMA);
+	p->addModel(modelGARCON);
 	p->addModel(modelVOID);
 }
